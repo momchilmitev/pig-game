321f8c3c287cf60cacea92fdebfc349ab098cd77
momchilmitev <momchilmitev87@gmail.com>
2020-11-28 18:00:54 +0100

    added selection of score and dice dom elements  with initial value

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting DOM elements
+const $score0 = document.getElementById('score--0');
+const $score1 = document.getElementById('score--1');
+const $dice = document.getElementsByClassName('dice')[0];
+
+// Inititalizing values
+$score0.textContent = 0;
+$score1.textContent = 0;
+$dice.classList.add('hidden');
