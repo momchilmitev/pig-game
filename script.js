18469c2dcf8d25402e3a87f248a683f94aed410a
momchilmitev <momchilmitev87@gmail.com>
2020-11-30 16:04:21 +0100

    added the logic for switching the players

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting DOM elements
+const $player0 = document.querySelector('.player--0');
+const $player1 = document.querySelector('.player--1');
 const $score0 = document.querySelector('#score--0');
 const $score1 = document.querySelector('#score--1');
 const $currentScore0 = document.querySelector('#current--0');
@@ -14,7 +16,9 @@ const $btnHold = document.querySelector('.btn--hold');
 $score0.textContent = 0;
 $score1.textContent = 0;
 $dice.classList.add('hidden');
+let totalScores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // The logic for rolling the dice
 $btnRoll.addEventListener('click', (e) => {
@@ -24,7 +28,14 @@ $btnRoll.addEventListener('click', (e) => {
 
   if (randomNumber !== 1) {
     currentScore += randomNumber;
-    $currentScore0.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    $player0.classList.toggle('player--active');
+    $player1.classList.toggle('player--active');
   }
 });
